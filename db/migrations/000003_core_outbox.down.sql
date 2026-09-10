37dfe50ef7f9523f0393882669d18891fbc81b41
Dmitry Krasulya <d.krasulya@getblogger.ru>
2026-09-10 17:45:13 +0300

    fix: remove conversation message foreign key before rollback

--- db/migrations/000003_core_outbox.down.sql
+++ db/migrations/000003_core_outbox.down.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS outbox_events;
 DROP TABLE IF EXISTS conversation_read_states;
 DROP TRIGGER IF EXISTS messages_conversation_channel_guard ON messages;
-DROP TABLE IF EXISTS messages;
 DROP TRIGGER IF EXISTS conversations_identity_channel_guard ON conversations;
+ALTER TABLE IF EXISTS conversations DROP CONSTRAINT IF EXISTS conversations_waiting_closed_message_fk;
+DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS conversations;
 DROP FUNCTION IF EXISTS verify_message_conversation_channel();
 DROP FUNCTION IF EXISTS verify_conversation_identity_channel();
